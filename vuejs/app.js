6d51c0cca6135a241b32b6f0223d917f72df6483
Vitor <vitor.thomazini94@gmail.com>
2018-12-08 15:20:25 -0200

    Computed Properties
    - The page process is partial

--- vuejs/app.js
+++ vuejs/app.js
@@ -3,9 +3,28 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: '',
-    age: ''
+    age: 20,
+    a: 0,
+    b: 0
   },
   methods: {
+    /*addToA: function() {
+      console.log('addToA');
+      return this.age + this.a;
+    },
+    addToB: function() {
+      console.log('addToB');
+      return this.age + this.b;
+    }*/
+  },
+  computed: {
+    addToA: function() {
+      console.log('addToA');
+      return this.age + this.a;
+    },
+    addToB: function() {
+      console.log('addToB');
+      return this.age + this.b;
+    }
   }
 });
