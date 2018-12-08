fe70a76e12976f44dd5091a7064d1245a00cd853
Vitor <vitor.thomazini94@gmail.com>
2018-12-08 20:12:16 -0200

    Conditionals

--- vuejs/app.js
+++ vuejs/app.js
@@ -3,18 +3,13 @@
 new Vue({
   el: '#vue-app',
   data: {
-    available: false,
-    nearby: false
+    error: false,
+    success: false
   },
   methods: {
 
   },
   computed: {
-    compClasses: function() {
-      return {
-        available: this.available,
-        nearby: this.nearby
-      }
-    }
+    
   }
 });
