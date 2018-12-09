9354829b6e831f67ded498d990f0fe67ffaa887d
Vitor <vitor.thomazini94@gmail.com>
2018-12-09 14:52:07 -0200

    Vue CLI
    - Vue Files
    - Root Component

---
+++ vuejs/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ greeting() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Your first vue file, wooo!'
+    }
+  },
+  methods: {
+    greeting: function() {
+      return 'Hello!!!'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
