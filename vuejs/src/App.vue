fd2bcbb66fc1915003bf1c2c1516d499063b9af8
Vitor <vitor.thomazini94@gmail.com>
2018-12-16 12:18:17 -0200

    Slots

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -1,40 +1,34 @@
-<!-- Reference Type: the variable is insert using props. If in any components it's edit, its change the original -->
 <template>
-  <div>
-    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
-    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
-    <app-footer v-bind:title="title"></app-footer>
-  </div>
+  <form-helper>
+    <div slot="form-header">
+      <h3>This is the title of the form</h3>
+      <p>Information about the form</p>
+    </div>
+    <div slot="form-fields">
+      <input type="text" placeholder="name" required/>
+      <input type="password" placeholder="password" required/>
+    </div>
+    <div slot="form-controls">
+      <button type="button" v-on:click="handleSubmit">Submit</button>
+    </div>
+  </form-helper>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
-import Ninjas from './components/Ninjas.vue'
+
+import FormHelper from './components/FormHelper.vue'
 
 export default {
   components: {
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-ninjas': Ninjas
+    'form-helper': FormHelper
   },
   data () {
     return {
-      ninjas: [
-        {name: 'Vitor', speciality: 'AI Programmer', show: false},
-        {name: 'Ryu', speciality: 'Vue Components', show: false},
-        {name: 'Hitoshi', speciality: 'Click Events', show: false},
-        {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-        {name: 'Tango', speciality: 'Conditionals', show: false},
-        {name: 'Kami', speciality: 'Webpack', show: false}
-      ],
-      title: 'Vue Ninjas'
+      title: 'I am a dynamic slot title'
     }
   },
   methods: {
-    updateTitle: function(title) {
-      this.title = title;
-    }
+
   }
 }
 </script>
