f92b4f7c5b0eea96a06bc4b0fd8bbceec012d03b
Vitor <vitor.thomazini94@gmail.com>
2018-12-16 14:42:41 -0200

    Dynamic Components

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <form-helper>
-    <div slot="form-header">
-      <h3>This is the title of the form</h3>
-      <p>Information about the form</p>
-    </div>
-    <div slot="form-fields">
-      <input type="text" placeholder="name" required/>
-      <input type="password" placeholder="password" required/>
-    </div>
-    <div slot="form-controls">
-      <button type="button" v-on:click="handleSubmit">Submit</button>
-    </div>
-  </form-helper>
+  <div>
+    <keep-alive>
+      <component v-bind:is="component"></component>
+    </keep-alive>
+    <button type="button" v-on:click="component = 'form-one'">Show form one</button>
+    <button type="button" v-on:click="component = 'form-two'">Show form two</button>
+  </div>
 </template>
 
 <script>
 
-import FormHelper from './components/FormHelper.vue'
+import FormOne from './components/FormOne.vue'
+import FormTwo from './components/FormTwo.vue'
 
 export default {
   components: {
-    'form-helper': FormHelper
+    'form-one': FormOne,
+    'form-two': FormTwo
   },
   data () {
     return {
-      title: 'I am a dynamic slot title'
+      component: 'form-one'
     }
   },
   methods: {
@@ -34,5 +30,8 @@ export default {
 </script>
 
 <style>
-
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+}
 </style>
---
+++ vuejs/src/components/FormTwo.vue
@@ -0,0 +1,45 @@
+<template>
+  <form-helper>
+    <div slot="form-header">
+      <h3>This is the title of the form</h3>
+      <p>Information about the form</p>
+    </div>
+    <div slot="form-fields">
+      <input type="text" placeholder="name" required/>
+      <input type="password" placeholder="password" required/>
+    </div>
+    <div slot="form-controls">
+      <button type="button" v-on:click="handleSubmit">Login</button>
+    </div>
+  </form-helper>
+</template>
+
+<script>
+
+import FormHelper from './FormHelper.vue'
+
+export default {
+  components: {
+    'form-helper': FormHelper
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    handleSubmit: function() {
+      alert('Thanks for logging in (form two)');
+    }
+  }
+}
+</script>
+
+<style lang="css">
+
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+}
+
+</style>
