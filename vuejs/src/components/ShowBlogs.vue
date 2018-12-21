93ced680d260ddeccbf25cacfa8c5815e9f8f847
Vitor <vitor.thomazini94@gmail.com>
2018-12-20 22:08:25 -0200

    Custom Search Filter

--- vuejs/src/components/ShowBlogs.vue
+++ vuejs/src/components/ShowBlogs.vue
@@ -1,7 +1,8 @@
 <template lang="html">
   <div id="show-blogs">
     <h1>All Blog Articles</h1>
-    <div v-for="blog in blogs" class="single-blog">
+    <input type="text" v-model="search" placeholder="Search blogs"/>
+    <div v-for="blog in filteredBlogs" class="single-blog">
       <h2>{{ blog.title | to-uppercase}}</h2>
       <article>{{ blog.body | snippet }}</article>
     </div>
@@ -12,7 +13,8 @@
 export default {
   data() {
     return {
-      blogs: []
+      blogs: [],
+      search: ''
     }
   },
   created(){
@@ -20,6 +22,13 @@ export default {
       console.log(data);
       this.blogs = data.body.slice(0,10);
     });
+  },
+  computed: {
+    filteredBlogs: function() {
+      return this.blogs.filter((blog) => {
+        return blog.title.match(this.search);
+      })
+    }
   }
 }
 </script>
