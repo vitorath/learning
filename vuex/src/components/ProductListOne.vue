2bbe523e135362120e6612c01f7321fb3afd36da
Vitor <vitor.thomazini94@gmail.com>
2018-12-28 15:00:45 -0200

    Mutations

--- vuex/src/components/ProductListOne.vue
+++ vuex/src/components/ProductListOne.vue
@@ -7,6 +7,7 @@
             <span class="price"> R${{ product.price }}</span>
           </li>
         </ul>
+        <button type="button" v-on:click="reducePrice">Reduce Price</button>
     </div>
 </template>
 
@@ -18,6 +19,14 @@ export default {
     },
     saleProducts() {
       return this.$store.getters.saleProducts;
+    }
+  },
+  methods: {
+    reducePrice: function() {
+      /* this.$store.state.products.forEach( product => {
+        product.price -= 1;
+      }) */
+      this.$store.commit('reducePrice');
     }
   }
 }
