3ec03251a716483efcb00dc8126ed1727d58f7df
Vitor <vitor.thomazini94@gmail.com>
2018-12-28 12:52:52 -0200

    Setting up a Central Store

---
+++ vuex/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
+  </div>
+</template>
+<script>
+import ProductListOne from './components/ProductListOne.vue'
+import ProductListTwo from './components/ProductListTwo.vue'
+
+export default {
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+<style>
+body {
+  font-family: Ubuntu;
+  color: #555;
+}
+</style>
