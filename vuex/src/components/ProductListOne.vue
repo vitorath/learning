5be1e0207c8cc7afe51897cebd28032c94765960
Vitor <vitor.thomazini94@gmail.com>
2018-12-28 16:49:13 -0200

    Mapping Actions & Getters

--- vuex/src/components/ProductListOne.vue
+++ vuex/src/components/ProductListOne.vue
@@ -12,22 +12,22 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+import { mapGetters } from 'vuex'
+
 export default {
   computed: {
     products() {
       return this.$store.state.products;
     },
-    saleProducts() {
-      return this.$store.getters.saleProducts;
-    }
+    ...mapGetters([
+      'saleProducts',
+    ])
   },
   methods: {
-    reducePrice: function(amount) {
-      /* this.$store.state.products.forEach( product => {
-        product.price -= 1;
-      }) */
-      this.$store.dispatch('reducePrice', amount);
-    }
+    ...mapActions([
+      'reducePrice'
+    ])
   }
 }
 </script>
