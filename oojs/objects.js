5c8e115380326d89a2bac1edd175040aea259d25
Vitor <vitor.thomazini94@gmail.com>
2018-12-31 17:08:06 -0200

    Updating Properties

--- oojs/objects.js
+++ oojs/objects.js
@@ -10,3 +10,14 @@ var userOne = {
 };
 
 console.log(userOne.name);
+userOne.name = 'Mario';
+console.log(userOne.name);
+userOne['name'] = 'Vitor';
+console.log(userOne.name);
+
+var prop = 'name';
+console.log(userOne[prop]);
+
+userOne.age = 25;
+userOne.logInfo = function() {};
+console.log(userOne);
