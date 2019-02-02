77cb75bec3847298d7ceb3265430ee373ab955dc
Vitor A Thomazini <vitor.thomazini94@gmail.com>
2019-02-02 16:17:51 -0200

    Generating WAR file for Tomcat

--- restfull webservice - spring boot/mobile-app-ws/src/main/java/app/ws/MobileAppWsApplication.java
+++ restfull webservice - spring boot/mobile-app-ws/src/main/java/app/ws/MobileAppWsApplication.java
@@ -2,13 +2,20 @@ package app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import app.ws.security.AppProperties;
 
 @SpringBootApplication
-public class MobileAppWsApplication {
+public class MobileAppWsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MobileAppWsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
